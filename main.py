20508ccbaedbcd4ece0e106c033ed4168ac3d1e2
Pawcio1235 <166720156+Pawcio1235@users.noreply.github.com>
2024-04-18 14:25:21 +0200

    feat:added search user

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from utils.crud import read
+from utils.crud import read, create_user, search
 from models.data import users
 
 if __name__ == '__main__':
@@ -8,8 +8,14 @@ if __name__ == '__main__':
         print("Menu:")
         print("0. Zakończ program")
         print("1.Pokaż co u znajommych: ")
+        print("2.Dodaj znajomego: ")
+        print("3.Wyszukaj znjaomego :")
         menu_option: str = input("Wybierz dostępną funkje z menu: ")
         if menu_option == "0":
             break
         if menu_option == "1":
             read(users)
+        if menu_option == "2":
+            create_user(users)
+        if menu_option == "3":
+            search(users)
